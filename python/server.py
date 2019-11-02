835db5ba049b7162ae9ede4a2f4bc471c4aafc9f
Avinash <hatman290@gmail.com>
2019-11-02 21:56:41 +0530

    localhost server program

---
+++ python/server.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from time import sleep
+import serial
+
+ser = serial.Serial('com3', 9600) # Establish the connection on a specific port
+counter = 32 # Below 32 everything in ASCII is gibberish
+PORT_NUMBER = 8080
+
+#Sensor sensor
+def sensor_output():
+    output = ser.readline() # Read the newest output from the Arduino
+    if counter == 255:
+        counter = 32
+    return output
+
+#This class will handles any incoming request from
+#the browser
+class myHandler(BaseHTTPRequestHandler):
+
+	#Handler for the GET requests
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		# Send the html message
+		self.wfile.write(sensor_output())
+		return
+
+try:
+	#Create a web server and define the handler to manage the
+	#incoming request
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print 'Started httpserver on port ' , PORT_NUMBER
+
+	#Wait forever for incoming htto requests
+	server.serve_forever()
+
+except KeyboardInterrupt:
+	print '^C received, shutting down the web server'
+	server.socket.close()
